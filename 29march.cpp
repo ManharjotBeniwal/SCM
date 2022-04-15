0327687be5e31b7a84c2445e074e33cb2c892537
Manharjot <manharjot0846.be21@chitkara.edu.in>
2022-04-15 10:52:25 +0530

    Hello

---
+++ 29march.cpp
@@ -0,0 +1,42 @@
+//1111111111111111111
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int a [] = {1,2,3,4,5,6};
+//     int*ptr;
+//     ptr = a;
+//     // cout<<ptr;
+//     // cout<<ptr+1;
+//     // cout<<ptr+3;
+//     // cout<<ptr[1];
+//     cout<<*a+4;
+//     return 0 ;
+// }
+//22222222222222222222
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int b=0;
+//     int*ptr=NULL;
+//     ptr=&b;
+//     cout<<*ptr;
+//     return 0 ;
+    
+// }
+//33333333333333333
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int b;
+//     int*ptr;
+//     ptr=&b;
+//     cout<<*ptr;
+//     return 0 ;
+    
+}
+//44444444444444444
+#include <iostream>
+using namespace std;
+int main(){
+    
+}
